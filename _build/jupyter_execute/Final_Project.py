c06063b8bca62ff059057cf71bef4245028dae6d
Jason Day <jason.day@kroger.com>
2022-11-28 22:11:41 -0500

    First book try

---
+++ _build/jupyter_execute/Final_Project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Final Project 
+
+# Authors:
+# - Jashani De Silva
+# - Elayna Berry
+# - Jason Day
+
+# ## Introduction
+
+# ### Required Packages
+
+# <mark> add packages </mark>
